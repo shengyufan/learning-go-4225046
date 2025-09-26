1b5860d5c71115ec7b7599ee680452e7a4d058d3
Yufan Sheng <18829237653@163.com>
2025-09-26 03:10:23 +0000

    practice for simple calculation

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Math")
+
+	i1, i2, i3 := 22, 33, 44
+	fmt.Println("sum: ", i1+i2+i3)
+	f1, f2, f3 := 22.2, 33.3, 44.4
+	fmt.Println("sum: ", f1+f2+f3)
+
+	// go 不支持隐式转换，因此进行数学计算时，需要保证类型相同
+	total := float64(i1) + f2
+	fmt.Println("sum: ", total)
+
 }
