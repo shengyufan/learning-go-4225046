de26658c54f9fa18dcb103bf914ea244eabd9431
Yufan Sheng <18829237653@163.com>
2025-09-26 02:46:32 +0000

    init variable chapter

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
 
-	fmt.Println("Hello from Go!")
+	// fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+
+	testNum := 42
+	// print the integer number
+	fmt.Printf("Test Number is: %v\n", testNum)
+	// print the data type
+	fmt.Printf("Test Data Type is: %T\n", testNum)
 }
