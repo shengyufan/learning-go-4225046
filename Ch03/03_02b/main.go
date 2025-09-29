57d8d2d7b3c0b7139065ddb1ab806af74b0a757b
Yufan Sheng <18829237653@163.com>
2025-09-29 03:52:33 +0000

    practice for pointer basic knowledge

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+
+	num := 42
+	var p *int = &num
+
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("Value of p:", *p)
+	}
+
+	val1 := 42.13
+	p1 := &val1
+	fmt.Println("Value 1 is", *p1)
+
+	// modify the value through the pointer
+	*p1 = *p1 / 31
+	fmt.Println("New value is", val1)
 }
