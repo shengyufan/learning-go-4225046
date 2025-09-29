de429fd269f6f20faf9fed6be8bc696a2499477e
Yufan Sheng <18829237653@163.com>
2025-09-29 03:58:32 +0000

    practice for array

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+
+	// array size cannot be extended after initialization
+	// init method 1
+	var colors [3]string
+	colors[0] = "red"
+	colors[1] = "green"
+	colors[2] = "blue"
+	fmt.Println(colors)
+
+	// init method 2
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+	fmt.Println("the size of the number array is", len(numbers))
 }
