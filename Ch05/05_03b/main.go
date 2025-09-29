c95b119b398ab001c9ebef1c5b1554b63b9639cc
Yufan Sheng <18829237653@163.com>
2025-09-29 07:17:11 +0000

    practice for basic goroutines

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	// Goroutine runs in the background (multiple threads)
 	fmt.Println("Goroutines")
+	go say("Hello to use Goroutine")
+	fmt.Println("Hello!")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("Helloooooo!")
+
+	time.Sleep(2 * time.Second)
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
