a50d2aefab60587975fd091adca52473d32f67d6
Yufan Sheng <18829237653@163.com>
2025-09-29 04:48:21 +0000

    practice for struct

--- Ch03/03_06b/main.go
+++ Ch03/03_06b/main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	fmt.Println("Structs")
+
+	poodle := Dog{"Poodle", 34}
+	fmt.Println(poodle)
+
+	fmt.Printf("Breed: %v, Weight: %v\n", poodle.Breed, poodle.Weight)
+	// +v represents including the field name and the corresponding value at the same time
+	fmt.Printf("%+v\n", poodle)
+
+	// change the value
+	poodle.Weight = 29
+	fmt.Println(poodle)
+}
+
+// define the struct
+type Dog struct {
+	Breed  string
+	Weight int
 }
