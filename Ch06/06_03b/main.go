b5600ceed886b333b0389c1e1fe5930ba7092cb7
Yufan Sheng <18829237653@163.com>
2025-09-29 08:10:43 +0000

    practice for handling web text

--- Ch06/06_03b/main.go
+++ Ch06/06_03b/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	content := readHttpContent()
-	fmt.Print(content)
+	// fmt.Print(content)
+	toursInfo := toursFromJSON(content)
+	for _, tour := range toursInfo {
+		price, _ := strconv.ParseFloat(strings.TrimSpace(tour.Price), 64)
+		fmt.Printf("%v: ($%v)\n", tour.Name, price)
+	}
+}
+
+func toursFromJSON(content string) []Tour {
+	tours := make([]Tour, 0)
+	decoder := json.NewDecoder(strings.NewReader(content))
+	// make sure the format is correct to prevent the error
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+
+	return tours
 }
 
 func readHttpContent() string {
@@ -37,4 +62,9 @@ func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type Tour struct {
+	Name  string
+	Price string
 }
