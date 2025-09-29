7ad796938e02c6de399860484477b524a9f6020d
Yufan Sheng <18829237653@163.com>
2025-09-29 06:09:41 +0000

    practice for if logic statement

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Conditional logic")
+
+	answer := 42
+	var res string
+
+	if answer < 0 {
+		res = "less than zero"
+	} else if answer == 0 {
+		res = "equal to zero"
+	} else {
+		res = "greater than zero"
+	}
+
+	fmt.Println(res)
 }
