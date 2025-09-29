70a963936adad3db431b18e142db1f2c80000989
Yufan Sheng <18829237653@163.com>
2025-09-29 04:26:52 +0000

    practice for map (init, add, delete, iterate)

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	fmt.Println("Maps")
+
+	// map[typeA]typeB defines a map using typeA param as the key and typeB param as the value
+	states := make(map[string]string)
+	states["NSW"] = "New South Wales"
+	states["QLD"] = "Queensland"
+	states["VIC"] = "Victoria"
+	fmt.Println(states)
+
+	queensland := states["QLD"]
+	fmt.Println(queensland)
+
+	// delete the item
+	delete(states, "QLD")
+	fmt.Println(states)
+
+	// add the item
+	states["WA"] = "Western Australia"
+	fmt.Println(states)
+
+	// iterate the map (different order than adding)
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	// iterate the map in specified order
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+
+	sort.Strings(keys)
+	fmt.Println("keys sorted")
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
