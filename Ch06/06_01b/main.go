1708508040643fbaef92dc98ff83cd843a1d2d6e
Yufan Sheng <18829237653@163.com>
2025-09-29 07:52:57 +0000

    practice for writing and reading local files

--- Ch06/06_01b/main.go
+++ Ch06/06_01b/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	fmt.Println("Files")
+	fileName := "./fromString.txt"
+	file, err := os.Create(fileName)
+	checkError(err)
+
+	// Defer the closing of the file. This will happen after the function returns.
+	defer file.Close()
+	
+	length, err := io.WriteString(file, "Hello from Go!")
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	readFile(fileName)
+}
+
+func checkError(err error) {
+	if err != nil {
+		// stop execution and output the error message
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	println("Data:", string(data))
 }
