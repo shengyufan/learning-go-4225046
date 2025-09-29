6add86a900a9c324f2bdec6a6d0915e585da6475
Yufan Sheng <18829237653@163.com>
2025-09-29 07:08:31 +0000

    practice for method

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -6,10 +6,20 @@ import (
 
 func main() {
 	dog := Dog{"Poodle", "Woof"}
-	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+	dog.Speak()
+	println(dog.SpeakThreeTimes())
 }
 
 type Dog struct {
-   Breed string
-   Sound string
+	Breed string
+	Sound string
+}
+
+func (d Dog) Speak() {
+	fmt.Printf("The %v says %v!\n", d.Breed, d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() string {
+	// Sprintf() return a variable
+	return fmt.Sprintf("%v! %v! %v!", d.Sound, d.Sound, d.Sound)
 }
